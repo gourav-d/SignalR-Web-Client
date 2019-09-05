bf47adb108969f6c42103b00d4dd886f47e1dd24
Gourav Dwivedi <Gourav.Dwivedi@emc.com>
2019-09-05 23:45:15 +0530

    Added Test Hub to dotnet core sample application

---
+++ Samples/AspDotCore/WebApp/WebApp/Hubs/OneHub.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Threading.Tasks;
+
+namespace WebApp.Hubs
+{
+	public class OneHub : Hub
+	{
+		public override Task OnConnectedAsync()
+		{
+			return base.OnConnectedAsync();
+		}
+
+		public async Task NotifySameClient(string data)
+		{
+			var connectionId = Context.ConnectionId;
+			await Clients.Client(connectionId).SendAsync("ReceiveData", $"Data Received: {data}");
+		}
+
+		public async Task NotifyAllClient(string data)
+		{
+			await Clients.All.SendAsync("ReceiveData", $"Data Received: {data}");
+		}
+
+		public async Task TestComplexData(string data)
+		{
+			var connectionId = Context.ConnectionId;
+			await Clients.Client(connectionId).SendAsync("ReceiveData", $"Data Received: {data}");
+		}
+
+		public override Task OnDisconnectedAsync(Exception exception)
+		{
+			return base.OnDisconnectedAsync(exception);
+		}
+	}
+}
