1771e553ec6c10a0a3abef95f28a676022502d7b
Gourav Dwivedi <Gourav.Dwivedi@dell.com>
2021-09-16 00:24:15 +0530

    syntax error fixed

--- Samples/AspDotCore/WebApp_3.1/WebApp/WebApp/Program.cs
+++ Samples/AspDotCore/WebApp_3.1/WebApp/WebApp/Program.cs
@@ -1,11 +1,5 @@
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace WebApp
 {
@@ -22,4 +16,4 @@ namespace WebApp
 				{
 					webBuilder.UseStartup<Startup>();
 				});
-	}k•
+	}}
